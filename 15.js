5e4ec7b29e4fb79e028a8af86daf244a3588ece5
erboerkinuly <120169581+erboerkinuly@users.noreply.github.com>
2022-12-09 15:49:09 +0600

    Add files via upload

---
+++ 15.js
@@ -0,0 +1,15 @@
+var text = "Satbayev university!";
+			var delay = 100; // cкорость
+			var elem = document.getElementById("result");
+			 
+			var print_text = function(text, elem, delay) {
+				if(text.length > 0) {
+					elem.innerHTML += text[0];
+					setTimeout(
+						function() {
+							print_text(text.slice(1), elem, delay); 
+						}, delay
+					);
+				}
+			}
+			print_text(text, elem, delay)
